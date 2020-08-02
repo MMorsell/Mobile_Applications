f7079c9533ec55a82bfd11aa9df0cb05f7971f49
martinmorsell18@gmail.com <martinmorsell18@gmail.com>
2020-08-02 14:41:12 +0200

    completed the basic application

--- MobileApps/Freerider/Freerider/Views/AllItemsPage.xaml.cs
+++ MobileApps/Freerider/Freerider/Views/AllItemsPage.xaml.cs
@@ -5,50 +5,24 @@ using Xamarin.Forms;
 using Freerider.Models;
 using Freerider.ViewModels;
 using Freerider.Services;
+using System.Threading.Tasks;
 
 namespace Freerider.Views
 {
     [DesignTimeVisible(false)]
     public partial class AllItemsPage : ContentPage
     {
-        private ItemsViewModel alreadyWatchedTrips;
-        private NotificationService _notificationService;
+        private ItemsViewModel _currentTripsOnHertzFreerider;
 
         public AllItemsPage()
         {
             InitializeComponent();
-            _notificationService = new NotificationService();
-            BindingContext = alreadyWatchedTrips = new ItemsViewModel();
+            BindingContext = _currentTripsOnHertzFreerider = new ItemsViewModel();
         }
 
-        private async void AddItem_Clicked(object sender, EventArgs e)
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushModalAsync(new NavigationPage(new AddItemPage()));
-        }
-
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-
-            //if (subscribeModels.Items.Count == 0)
-            //    subscribeModels.IsBusy = true;
-        }
-
-        public async void OnDelete(object sender, EventArgs e)
-        {
-            var mi = ((MenuItem)sender);
-            var currentSubscribePost = (ItemModel)mi.CommandParameter;
-            if (await DisplayAlert("Ta bort bevakning", $"Vill du verkligen ta bort bevakning:\n\n {currentSubscribePost.FormattedString}?", "Ja", "Nej"))
-            {
-                alreadyWatchedTrips.Items.Remove(alreadyWatchedTrips.Items.FirstOrDefault(sMod => sMod.Id.Equals(currentSubscribePost.Id)));
-            };
-        }
-
-        public void OnScheduleClick(object sender, EventArgs e)
-        {
-            string title = $"Local Notification #";
-            string message = $"You have now received notifications!";
-            _notificationService.SendNotification(title, message);
+            await Navigation.PushModalAsync(new NavigationPage(new AllSubscribedTrips()));
         }
     }
 }
---
+++ MobileApps/Freerider/Freerider/Views/AllSubscribedTrips.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Freerider.Models;
+using Freerider.Services;
+using Freerider.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Freerider.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllSubscribedTrips : ContentPage
+    {
+        private NotificationService _notificationService;
+        private ItemsViewModel alreadyWatchedTrips;
+
+        public AllSubscribedTrips()
+        {
+            InitializeComponent();
+            _notificationService = new NotificationService();
+        }
+
+        private async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new AddItemPage()));
+        }
+
+        public async void OnDelete(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            var currentSubscribePost = (ItemModel)mi.CommandParameter;
+            if (await DisplayAlert("Ta bort bevakning", $"Vill du verkligen ta bort bevakning:\n\n {currentSubscribePost.FormattedString}?", "Ja", "Nej"))
+            {
+                alreadyWatchedTrips.Items.Remove(alreadyWatchedTrips.Items.FirstOrDefault(sMod => sMod.Id.Equals(currentSubscribePost.Id)));
+            };
+        }
+
+        public void OnScheduleClick(object sender, EventArgs e)
+        {
+            string title = $"Local Notification #";
+            string message = $"You have now received notifications!";
+            _notificationService.SendNotification(title, message);
+        }
+    }
+}
